e9682b1e7a3e3b9123122e56bf17aed66d9836bd
panno <simopanini@gmail.com>
2024-03-19 22:47:41 +0100

    ooollkl

--- src/index.py
+++ src/index.py
@@ -1,6 +1,7 @@
 import requests
 import pandas
 import json
+import numpy as np
 AV_KEY = "NPRZ3R6XPBJWSA26"
 
 
@@ -35,7 +36,71 @@ def jsonToXlsx(data, jsonFile="jsonFile.json", excelFile="excelFile.xlsx"):
     return dataPd.to_excel(DATA_DIR+excelFile)
 
 
-jsonToXlsx(getBIMarketData("DE000VU32806"))
-jsonToXlsx(calculateDelta(getBIMarketData("DE000VU32806")), "jsonDeltaFile.json", "excelDeltaFile.xlsx")
+class MarketMatrix:
+    def __init__(self, marketList):
+        self.marketList = marketList
+        mat = [[0.]*len(marketList)]*len(marketList)
+        self.correlationMatrix = np.array(mat)
+        self.betaMatrix = np.array(mat)
+        self.marketData = {}
+        
+    def loadMarketData(self):
+        for m in self.marketList:
+            self.marketData[m] = calculateDelta(getBIMarketData(m))
+            print(f'getting {m}, l: {len(self.marketData[m])}')
+            
+    def calculateCorrelation(self):
+        marketNum = len(self.marketList)
+        for m0 in range(marketNum):
+            m0Name = self.marketList[m0]
+            for m1 in range(marketNum):
+                m1Name = self.marketList[m1]
+                if(m1!=m0):
+                    if(len(self.marketData[m0Name]) > 0 and len(self.marketData[m1Name]) > 0):
+                        dataLen = min(len(self.marketData[m0Name]), len(self.marketData[m1Name]))
+                
+                        self.correlationMatrix[m0][m1] = round(np.corrcoef([np.array([v["delta"] for v in self.marketData[m0Name][-dataLen:]]), np.array([v["delta"] for v in self.marketData[m1Name][-dataLen:]])])[0][1], 3)
+                    else:
+                        self.correlationMatrix[m0][m1] = -2
+                else:
+                    self.correlationMatrix[m0][m1] = 1
+    
+    def calculateBetas(self):
+        marketNum = len(self.marketList)
+        for m0 in range(marketNum):
+            m0Name = self.marketList[m0]
+            for m1 in range(marketNum):
+                m1Name = self.marketList[m1]
+                if(len(self.marketData[m0Name]) > 0 and len(self.marketData[m1Name]) > 0):
+                    dataLen = min(len(self.marketData[m0Name]), len(self.marketData[m1Name]))
+                    print(np.var( np.array([v["delta"] for v in self.marketData[m1Name][-dataLen:]])))
+                    self.betaMatrix[m0][m1] = round(np.cov([np.array([v["delta"] for v in self.marketData[m0Name][-dataLen:]]), np.array([v["delta"] for v in self.marketData[m1Name][-dataLen:]])])[0][1]/np.cov([np.array([v["delta"] for v in self.marketData[m1Name][-dataLen:]]), np.array([v["delta"] for v in self.marketData[m1Name][-dataLen:]])])[0][1], 3)
+                else:
+                    self.betaMatrix[m0][m1] = -2
 
+        pass
+        
+
+
+
+#jsonToXlsx(getBIMarketData("DE000VU32806"))
+#jsonToXlsx(calculateDelta(getBIMarketData("DE000VU32806")), "jsonDeltaFile.json", "excelDeltaFile.xlsx")
 #print(getUSMarketData("TIME_SERIES_DAILY", "IBM", "outputsize=full"))
+    
+MARKET_NUM = 5
+
+fileIn = open("resources/marketList.txt", "r")
+l = fileIn.read().split("\n")
+fileIn.close()
+mm = MarketMatrix(l[:MARKET_NUM])
+mm.loadMarketData()
+mm.calculateCorrelation()
+mm.calculateBetas()
+fileOut = open("data/matrix.txt", "w")
+fileOut.write("coef corr\n")
+fileOut.write(str(mm.correlationMatrix))
+fileOut.write("\n\nbetas\n")
+fileOut.write(str(mm.betaMatrix))
+jsonToXlsx([mm.marketList] + mm.correlationMatrix.tolist(), excelFile="correlation.xlsx")
+jsonToXlsx([mm.marketList] + mm.betaMatrix.tolist(), excelFile="beta.xlsx")
+fileOut.close()
